cf7dafb09ea20ef9be952d06094c1eb9f20cdf33
Curtis <chender93@gmail.com>
2020-12-13 23:57:22 -0500

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# altgather
-A NoSQL social media back-end application that utilizes Express and MongoDB
+# Social Media API
+A NoSQL social media back-end application that utilizes:
+- Node.Js
+- Express
+- MongoDB
+- Mongoose
+## Walkthrough Video
+[![Walkthrough](http://img.youtube.com/vi/X_khphNiymo/0.jpg)](http://www.youtube.com/watch?v=X_khphNiymo "Walkthrough")
+
+## Overview
+This applicaiton allows users to create, read, update, and delete thoughts and reactions in a social platform-like setting. This back-end application can be attached to a front-end view for users to enjoy fully.
+
+## Installation
+Be sure that you have installed Node.js, express, MongoDB, and Mongoose on your local computer. Clone this repo to your computer, install dependencies by running the command, 'npm install' in your CLI, and start the server by running the command 'npm start'.
+
+You can view my GitHub repo [Here](https://github.com/chender93/social-network)
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,13 +4,9 @@ const userController = {
     // GET all Users
     getAllUser(req, res) {
         User.find({})
-            .populate({
-                path: 'Thought',
-                select: '-__v'
-            })
             .select('-__v')
-            .sort({ _id: -1 })
-            .then(dbUserData => res.json(dbUserData))
+            .sort({ createdAt: 'desc' })
+            .then(userData => res.json(userData))
             .catch(err => {
                 console.log(err);
                 res.status(400).json(err);
@@ -20,55 +16,59 @@ const userController = {
     // GET a single user by its _id
     getUserById({ params }, res) {
         User.findOne({ _id: params.id })
-            .populate({
-                path: 'Thought',
-                select: '-__v'
-            })
+            .populate({ path: 'thoughts', select: '-__v' })
+            .populate({ path: 'friends', select: '-__v' })
             .select('-__v')
-            .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No User found with this id!' });
+            .sort({ createdAt: 'desc' })
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with that ID.' });
                     return;
                 }
-                res.json(dbUserData);
-            })
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
+                res.json(userData);
             });
     },
 
     // POST a new user 
     createUser({ body }, res) {
         User.create(body)
-            .then(dbUserData => res.json(dbUserData))
-            .catch(err => res.status(400).json(err));
-    },
-
-    // PUT to update a User by its _id
-    updateUser({ params }, res) {
-        User.findOneAndUpdate({ _id: params.id })
-            .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No User found with this id!' });
-                    return;
-                }
-                res.json(dbUserData);
+            .then(userData => {
+                res.json(userData);
             })
             .catch(err => res.status(400).json(err));
     },
 
-    
+    // PUT to update a User by its _id
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .select('-__v')
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with that ID.' });
+                    return;
+                }
+                res.json({ message: 'The user was updated.', userData });
+            })
+            .catch(err => res.status(400).json(err));
+    },
+
+
 
     // delete to remove User by its _id
     deleteUser({ params }, res) {
         User.findOneAndDelete({ _id: params.id })
-            .then(dbUserData => {
-                if (!dbUserData) {
-                    res.status(404).json({ message: 'No User found with this id!' });
-                    return;
+            .select('-__v')
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with that ID.' });
                 }
-                res.json(dbUserData);
+                return Thought.deleteMany(
+                    { username: userData.username },
+                    { new: true, runValidators: true }
+                )
+                    .then(thoughtData => {
+                        res.json({ message: `This user has been deleted.`, userData, thoughtData });
+                    });
             })
             .catch(err => res.status(400).json(err));
     },
@@ -78,10 +78,17 @@ const userController = {
         User.findOneAndUpdate(
             { _id: params.userId },
             { $push: { friends: params.friendId } },
-            { new: true }
+            { new: true, runValidators: true }
         )
-            .then(dbFriendData => res.json(dbFriendData))
-            .catch(err => res.json(err));
+            .select('-__v')
+            .then(userData => {
+                if (!userData) {
+                    res.status(404).json({ message: 'No user found with that ID.' });
+                    return;
+                }
+                res.json({ message: `The user was added to your friend list.`, userData });
+            })
+            .catch(err => res.status(400).json(err));
     },
 
     // DELETE to remove a friend from a user's friend list
